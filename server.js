22510ea244f395fdf8cad6a091f7b1d5cc8e08a0
jaewon-ho <jwho5073@gmail.com>
2022-04-29 09:50:18 +0900

    몽고 DB 커넥션 추가

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 
+global.conf = require('./config');
+
+const mongoose = require('mongoose');
+mongoose.connect(
+    global.config.mongodb.url, 
+    (err) => {
+        if(err) {
+            console.error('mongoDB connection ERROR');
+            console.errpr(err);
+        } else {
+            console.log('mongoDB connection created')
+        }
+    }
+);
+
 const PORT = 4000;
 
 app.use(express.json());
